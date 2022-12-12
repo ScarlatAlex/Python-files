9ae730587142835f62692478c5fd339616967bb1
Alex <gitallex@mail.ru>
2022-12-12 12:31:11 +0200

    Create Modules.py

---
+++ Modules.py
@@ -0,0 +1,14 @@
+# modulul este un file sau mai multe file-uri ce le putem conecta la proiectul nostru si sa adaugam functii noi
+
+# import time
+
+# time.sleep(3)
+# print("Hello")
+
+import datetime as d, sys, os, platform
+import random, array, math
+# datetime or d
+print(d.datetime.now().time())
+print(platform.system())
+
+
